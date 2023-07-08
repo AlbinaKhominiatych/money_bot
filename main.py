247a4907cb173a55eb5c985990506685e858a881
AlbinaHominyatich <hominyatichalbina@gmail.com>
2023-07-08 20:21:55 +0300

    додавання повного виводу інформації

--- main.py
+++ main.py
@@ -9,21 +9,20 @@ bot = telebot.TeleBot("6272635544:AAHfs2PbPaHHBWuEVdDzrA7r-7U_fnGG3h4")
 def get_currency_rates():
     response1 = requests.get("https://minfin.com.ua/ua/currency/usd/")
     soup1 = BeautifulSoup(response1.text, features="html.parser")
-    for usd_rate in soup1.find('div', {'class': "sc-1x32wa2-9 bKmKjX"}):
-        usd_rate.extract()
-
+    usd_elements = soup1.find_all('div', {'class': "sc-1x32wa2-9 bKmKjX"})
+    usd_values = [element.text.strip() for element in usd_elements]
 
     response2 = requests.get("https://minfin.com.ua/ua/currency/eur/")
     soup2 = BeautifulSoup(response2.text, features="html.parser")
-    for eur_rate in soup2.find('div', {'class': "sc-1x32wa2-9 bKmKjX"}):
-        eur_rate.extract()
+    eur_elements = soup2.find_all('div', {'class': "sc-1x32wa2-9 bKmKjX"})
+    eur_values = [element.text.strip() for element in eur_elements]
 
     response3 = requests.get("https://minfin.com.ua/ua/currency/pln/")
     soup3 = BeautifulSoup(response3.text, features="html.parser")
-    for pln_rate in soup3.find('div', {'class': "sc-1x32wa2-9 bKmKjX"}):
-        pln_rate.extract()
+    pln_elements = soup3.find_all('div', {'class': "sc-1x32wa2-9 bKmKjX"})
+    pln_values = [element.text.strip() for element in pln_elements]
 
-    return usd_rate, eur_rate, pln_rate
+    return usd_values, eur_values, pln_values
 
 ukr_month_names = {
     1: "січня",
@@ -57,17 +56,38 @@ def start(message):
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     if message.text.strip() == "Курс Долара":
-        usd_rate, _, _ = get_currency_rates()
-        answer = "Курс Долара становить {}".format(usd_rate)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton("Купівля")
+        item2 = types.KeyboardButton("Продаж")
+        item3 = types.KeyboardButton("Курс НБУ")
+        markup.add(item1, item2, item3)
+
+        usd_values, _, _ = get_currency_rates()
+        answer = "Курс Долара:\n\nКупівля: {}\nПродаж: {}\nКурс НБУ: {}".format(usd_values[0], usd_values[1], usd_values[2])
+
     elif message.text.strip() == "Курс Євро":
-        _, eur_rate, _ = get_currency_rates()
-        answer = "Курс Євро становить {}".format(eur_rate)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton("Купівля")
+        item2 = types.KeyboardButton("Продаж")
+        item3 = types.KeyboardButton("Курс НБУ")
+        markup.add(item1, item2, item3)
+
+        _, eur_values, _ = get_currency_rates()
+        answer = "Курс Євро:\n\nКупівля: {}\nПродаж: {}\nКурс НБУ: {}".format(eur_values[0], eur_values[1], eur_values[2])
+
     elif message.text.strip() == "Курс Злотих":
-        _, _, pln_rate = get_currency_rates()
-        answer = "Курс Злотих становить {}".format(pln_rate)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton("Купівля")
+        item2 = types.KeyboardButton("Продаж")
+        item3 = types.KeyboardButton("Курс НБУ")
+        markup.add(item1, item2, item3)
+
+        _, _, pln_values = get_currency_rates()
+        answer = "Курс Злотих:\n\nКупівля: {}\nПродаж: {}\nКурс НБУ: {}".format(pln_values[0], pln_values[1], pln_values[2])
+
     else:
         answer = "Я не розумію вашого запиту."
 
-    bot.send_message(message.chat.id, answer)
+    bot.send_message(message.chat.id, answer, reply_markup=markup)
 
 bot.polling()
